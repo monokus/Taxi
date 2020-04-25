112765109154b01b9a041f364b6b6417cde0e0ef
Raúl Alberto Moncada Carrero <rmoncada_carrero@hotmail.com>
2020-04-24 22:55:45 -0400

    Begginign Mobil tasks, Hambuger Menu Added With 6 MenuItem Options, Navigation through maps and Api Key Added.

---
+++ Taxi.Common/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taxi.Common.Models
+{
+    public class Menu
+    {
+        public string Icon { set; get; }
+        public string Title { set; get; }
+        public string PageName { set; get; }
+
+    }
+}
